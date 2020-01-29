8e74757768dddb38cbe39f7c7cdb2eee1e5d6a0f
Daniel Menet <daniel.menet@swisstopo.ch>
2020-01-29 08:51:12 +0100

    fixed handler

--- go.mod
+++ go.mod
@@ -5,11 +5,11 @@ go 1.13
 require (
 	github.com/apex/gateway v1.1.1
 	github.com/aws/aws-lambda-go v1.13.3 // indirect
+	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/mux v1.6.2
 	github.com/justinas/alice v1.2.0
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sfreiberg/gotwilio v0.0.0-20200113224334-06f83df0c687
 	github.com/tj/assert v0.0.0-20190920132354-ee03d75cd160 // indirect
-	github.com/unprofession-al/routing v0.0.0-20181224101742-b7da83e1e135
 	gopkg.in/yaml.v2 v2.2.2
 )
--- handlers.go
+++ handlers.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -10,71 +9,29 @@ import (
 )
 
 func (s Server) HookHandler(res http.ResponseWriter, req *http.Request) {
-	fmt.Println("hook handler triggererd")
 	vars := mux.Vars(req)
 	hName, ok := vars["hook"]
 	if !ok {
 		s.respond(res, req, http.StatusNotFound, fmt.Sprintf("hook not provided"))
-		fmt.Println(fmt.Sprintf("hook not provided, %v", vars))
 		return
 	}
 
-	reqText, _ := json.Marshal(req)
-	fmt.Println(reqText)
-
 	body, err := ioutil.ReadAll(req.Body)
-	fmt.Println(body)
 	defer req.Body.Close()
 	if err != nil {
 		s.respond(res, req, http.StatusInternalServerError, fmt.Sprintf("could not read request body: %s", err.Error()))
-		fmt.Println(fmt.Sprintf("Body was trash"))
 		return
 	}
 
 	h, ok := s.hooks[hName]
 	if !ok {
 		s.respond(res, req, http.StatusNotFound, fmt.Sprintf("hook '%s' not defined", hName))
-		fmt.Println(fmt.Sprintf("hook '%s' not defined", hName))
 		return
 	}
 
 	err = h.Process(body)
 	if err != nil {
 		s.respond(res, req, http.StatusInternalServerError, fmt.Sprintf("could not process hook: %s", err.Error()))
-		fmt.Println(fmt.Sprintf("could not process hook: %s", err.Error()))
-		return
-	}
-
-	s.respond(res, req, http.StatusOK, "sent")
-}
-
-func (s Server) PingdomHookHandler(res http.ResponseWriter, req *http.Request) {
-	fmt.Println("hook handler triggererd")
-
-	reqText, _ := json.Marshal(req)
-	fmt.Println(reqText)
-
-	body, err := ioutil.ReadAll(req.Body)
-	fmt.Println(body)
-	defer req.Body.Close()
-	if err != nil {
-		s.respond(res, req, http.StatusInternalServerError, fmt.Sprintf("could not read request body: %s", err.Error()))
-		fmt.Println(fmt.Sprintf("Body was trash"))
-		return
-	}
-
-	hName := "pingdomSwisstopoToSms"
-	h, ok := s.hooks[hName]
-	if !ok {
-		s.respond(res, req, http.StatusNotFound, fmt.Sprintf("hook '%s' not defined", hName))
-		fmt.Println(fmt.Sprintf("hook '%s' not defined", hName))
-		return
-	}
-
-	err = h.Process(body)
-	if err != nil {
-		s.respond(res, req, http.StatusInternalServerError, fmt.Sprintf("could not process hook: %s", err.Error()))
-		fmt.Println(fmt.Sprintf("could not process hook: %s", err.Error()))
 		return
 	}
 
